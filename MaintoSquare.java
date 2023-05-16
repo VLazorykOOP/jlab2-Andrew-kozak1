ec910805bd4fb8dd3955cf8c54b99ce2da4ad84a
Andrew-kozak1 <125350155+Andrew-kozak1@users.noreply.github.com>
2023-05-16 19:17:44 +0300

    Update MaintoSquare.java

--- MaintoSquare.java
+++ MaintoSquare.java
@@ -1,6 +1,6 @@
 package lab22;
 import java.util.Scanner;
-public class Main {
+public class MaintoSquare {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
